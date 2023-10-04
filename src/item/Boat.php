19add3fe101967b6853a7a34bcfe387f1d8436ae
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2023-10-04 16:15:15 +1000

    Update Boat.php

--- src/item/Boat.php
+++ src/item/Boat.php
@@ -23,25 +23,55 @@ declare(strict_types=1);
 
 namespace pocketmine\item;
 
-class Boat extends Item{
-	private BoatType $boatType;
+use pocketmine\entity\Entity;
+usepocketmine\entity\EntityId;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\AddEntityPacket;
+use pocketmine\network\mcpe\protocol\RemoveEntityPacket;
+use pocketmine\Player;
 
-	public function __construct(ItemIdentifier $identifier, string $name, BoatType $boatType){
-		parent::__construct($identifier, $name);
-		$this->boatType = $boatType;
-	}
+class Boat {
+    public static function spawnBoat(Player $player, string $boatType) {
+        $boat = null;
+        $boatId = null;
 
-	public function getType() : BoatType{
-		return $this->boatType;
-	}
+        switch ($boatType) {
+            case "oak":
+                $boat = Entity::createEntity("Boat", $player->getLevel(), Entity::createBaseNBT($player->getPosition()));
+                $boatId = EntityIds::BOAT;
+                break;
+            case "spruce":
+                $boat = Entity::createEntity("SpruceBoat", $player->getLevel(), Entity::createBaseNBT($player->getPosition()));
+                $boatId = EntityIds::SPRUCE_BOAT;
+                break;
+            case "birch":
+                $boat = Entity::createEntity("BirchBoat", $player->getLevel(), Entity::createBaseNBT($player->getPosition()));
+                $boatId = EntityIds::BIRCH_BOAT;
+                break;
+            case "jungle":
+                $boat = Entity::createEntity("JungleBoat", $player->getLevel(), Entity::createBaseNBT($player->getPosition()));
+                $boatId = EntityIds::JUNGLE_BOAT;
+                break;
+            case "acacia":
+                $boat = Entity::createEntity("AcaciaBoat", $player->getLevel(), Entity::createBaseNBT($player->getPosition()));
+                $boatId = EntityIds::ACACIA_BOAT;
+                break;
+            case "darkoak":
+                $boat = Entity::createEntity("DarkOakBoat", $player->getLevel(), Entity::createBaseNBT($player->getPosition()));
+                $boatId = EntityIds::DARK_OAK_BOAT;
+                break;
+            default:
+                $player->sendMessage("Invalid boat type. Available types: oak, spruce, birch, jungle, acacia, darkoak");
+                return;
+        }
 
-	public function getFuelTime() : int{
-		return 1200; //400 in PC
-	}
-
-	public function getMaxStackSize() : int{
-		return 1;
-	}
-
-	//TODO
+        if ($boat !== null) {
+            $boat->spawnToAll();
+            $player->getInventory()->removeItem(Item::get(Item::BOAT, $boatId));
+            $player->sendMessage("Boat spawned!");
+        } else {
+            $player->sendMessage("Failed to spawn boat.");
+        }
+    }
 }
